8c7719c38f6983838956eb4684b3fbb0c395562c
MukheshRavi <mukeshparvathi@gmail.com>
2020-10-20 23:00:23 +0530

    [Mukhesh] Refactor Get maximum among Three Integers

---
+++ NonGeneric.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMaximum
+{
+    public class NonGeneric
+    {
+        /// <summary>
+        /// max of 3 integers using non generic method
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="third"></param>
+        /// <returns></returns>
+        public static int GetMaxIntNumberAmongThree(int first, int second, int third)
+        {
+            Console.WriteLine("Maximum number");
+            //compare to method is used to compare 2 numbers
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+            {
+                return third;
+            }
+            throw new Exception("All 3 numbers are same");
+        }
+    }
+}
